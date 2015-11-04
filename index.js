23f328afad4fb3aca8c47e41bc7546cfa2f0a41e
ken <ken198701@gmail.com>
2015-11-04 16:22:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+var template = require('./template.js');
+
+module.exports = function(content, file, settings) {
+    content = content.replace(/<\!--@@tmpl_start(:.*)?-->([\s\S]*?)<\!--@@tmpl_end-->/g, function(str, _1, _2) {
+        var jsonStr = _1,
+            json;
+        //是否捕获到
+        if (_1 && _1.length > 1) {
+            //是否是正确的json格式
+            try {
+                json = JSON.parse(_1.substr(1));
+            } catch (e) {
+                console.log("解析json字符串时出错！");
+                return _2;
+            }
+            return template.compile(_2)(json);
+        } else {
+            return _2;
+        }
+    });
+    return content;
+};
 
