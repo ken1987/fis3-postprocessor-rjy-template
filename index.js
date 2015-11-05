b2bb7fae4fced4fba59f9a46268936f7d3f7378d
ken <ken198701@gmail.com>
2015-11-05 13:07:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 var template = require('./template.js');
 
 module.exports = function(content, file, settings) {
-    content = content.replace(/<\!--@@tmpl_start(:.*)?-->([\s\S]*?)<\!--@@tmpl_end-->/g, function(str, _1, _2) {
-        var jsonStr = _1,
-            json;
-        //是否捕获到
-        if (_1 && _1.length > 1) {
-            //是否是正确的json格式
-            try {
-                json = JSON.parse(_1.substr(1));
-            } catch (e) {
-                console.log("解析json字符串时出错！");
-                return _2;
-            }
-            return template.compile(_2)(json);
-        } else {
-            return _2;
-        }
-    });
-    return content;
-};
+    var info = fis.uri('/test/config.json');
+    var configFile = fis.file(info.file.origin);
+    var json;
 
+    //配置文件存在
+    if (configFile.exists) {
+        //是否是正确的json格式
+        try {
+            json = JSON.parse(configFile.getContent());
+        } catch (e) {
+            json = {};
+            console.error("解析json字符串时出错！");
+        }
+    }
+    return content.replace(/<\!--@@tmpl_start:(.*?)-->([\s\S]*?)<\!--@@tmpl_end-->/g, function(str, _1, _2) {
+        return template.compile(_2)(json[_1]);
+    });
+};
