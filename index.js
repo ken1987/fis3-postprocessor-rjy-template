7e56d9801b0a5d49512b4dffab63bf7138346e4f
ken1987 <ken198701@gmail.com>
2015-11-05 15:55:05 +0800

    update

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var template = require('./template.js');
 
 module.exports = function(content, file, settings) {
-    var info = fis.uri('/test/config.json');
-    var configFile = fis.file(info.file.origin);
+    //获取配置文件路径信息
+    var path;
+    content.replace(/<\!--\s*tmpl-config=('|")([\s\S]*?)\1\s*-->/g, function(str, _1, _2) {
+        path = fis.uri(_2, file.dirname);
+        return "";
+    });
+    var configFile = fis.file(path.file.origin);
     var json;
 
     //配置文件存在
