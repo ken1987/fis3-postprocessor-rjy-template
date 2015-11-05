2af374c66a355a95f8d1341fdd7846aa08cc9553
ken1987 <ken198701@gmail.com>
2015-11-05 17:09:32 +0800

    update

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 var template = require('./template.js');
 
 module.exports = function(content, file, settings) {
-    //获取配置文件路径信息
+    var regConfig = /<\!--\s*tmpl-config=('|")([\s\S]*?)\1\s*-->/g; //匹配配置文件路径
+    var regContent = /<\!--@@tmpl_start:(.*?)-->([\s\S]*?)<\!--@@tmpl_end-->/g; //匹配内容
+    var json = {};
+    var configFile;
     var path;
-    content.replace(/<\!--\s*tmpl-config=('|")([\s\S]*?)\1\s*-->/g, function(str, _1, _2) {
-        path = fis.uri(_2, file.dirname);
+
+    //获取配置文件路径信息
+    content.replace(regConfig, function(str, _1, _2) {
+        path = _2;
         return "";
     });
-    var configFile = fis.file(path.file.origin);
-    var json;
 
-    //配置文件存在
-    if (configFile.exists) {
-        //是否是正确的json格式
-        try {
-            json = JSON.parse(configFile.getContent());
-        } catch (e) {
-            json = {};
-            console.error("解析json字符串时出错！");
+    //获取配置文件
+    if (path !== void 0) {
+        path = fis.uri(path, file.dirname);
+        configFile = fis.file(path && path.file ? path.file.origin : "");
+
+        //配置文件存在
+        if (configFile.exists()) {
+            //是否是正确的json格式
+            jsonStr = configFile.getContent();
+            try {
+                json = JSON.parse(jsonStr);
+            } catch (e) {
+                console.error("fis3-postprocessor-rjy-template：解析json字符串时出错！");
+            }
         }
     }
-    return content.replace(/<\!--@@tmpl_start:(.*?)-->([\s\S]*?)<\!--@@tmpl_end-->/g, function(str, _1, _2) {
+
+    return content.replace(regContent, function(str, _1, _2) {
         return template.compile(_2)(json[_1]);
     });
 };
