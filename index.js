f2031ffefc68192a564f98c9624c78c3c0b4d208
ken1987 <ken198701@gmail.com>
2015-11-25 14:40:24 +0800

    update

--- index.js
+++ index.js
@@ -3,23 +3,40 @@ var reg = /<\!--rjy_template_start:(.*?)-->([\s\S]*?)<\!--rjy_template_end-->/g;
 
 module.exports = function(content, file, settings) {
     return content.replace(reg, function(all, str, tmpl) {
+        //解析str
+        var options = {},
+            arr = str.split("&"),
+            i, l, n;
+        for (i = 0, l = arr.length; i < l; i++) {
+            n = arr[i].split("=");
+            options[n[0]] = n[1];
+        }
+
+        //如果存在文本，说明是简单数据
         var json, path, newFile;
-        //如果是url(path)格式，表示传入的是路径，否则是字符串
-        if (/^url\(.*\)/.test(str)) {
-            path = fis.uri(str.substr(4, str.length - 5), file.dirname); //获取绝对路径
+        if (options.text !== void 0) {
+            json = str;
+        } else if (options.url !== void 0) {
+            path = fis.uri(options.url, file.dirname); //获取绝对路径
             newFile = fis.file(path && path.file ? path.file.origin : ""); //获取文件
             if (newFile.exists()) {
                 try {
                     json = JSON.parse(newFile && newFile.getContent());
                 } catch (e) {
-                    console.error("\n fis3-postprocessor-rjy-template：解析json字符串时出错，请检查数据格式！");
+                    console.error("\n " + str + "：解析json字符串时出错，请检查数据格式！");
+                }
+
+                if (options.id !== void 0) {
+                    var ids = options.id.split(".");
+                    for (i = 0, l = ids.length; i < l; i++) {
+                        json = json && json[ids[i]];
+                    }
                 }
             }
-        } else {
-            json = str;
         }
-        if (json == void(0) || json === "") {
-            console.error("\n fis3-postprocessor-rjy-template：数据不存在！");
+
+        //解析数据
+        if (json == void(0)) {
             return tmpl;
         } else {
             return template.compile(tmpl)(json);
